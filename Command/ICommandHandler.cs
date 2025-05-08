122c9f4d4fc03b5064967cd6520117b4d0b30f0d
Filipp <f.ivashov@gmail.com>
2025-05-08 11:46:01 +0400

    add command pattern and rename

---
+++ Command/ICommandHandler.cs
@@ -0,0 +1,7 @@
+namespace Game.State.cmd
+{
+    public interface ICommandHandler<TCommand> where TCommand : ICommand
+    {
+        bool Handle(TCommand command);
+    }
+}
---
+++ Command/ICommandPipelineHook.cs
@@ -0,0 +1,9 @@
+using Game.State.cmd;
+
+namespace Command
+{
+    public interface ICommandPipelineHook 
+    {
+        void OnCommandHandled<TCommand>(TCommand command, bool success) where TCommand : ICommand;
+    }
+}
